4f8d9eb4f48bd068a56e16c4378c93f94d4f75a2
PHUNG VAN DOAN <phungvandoan2k3@gmail.com>
2024-09-17 21:40:30 +0700

    updated

--- 5_Quiz/src/App.js
+++ 5_Quiz/src/App.js
@@ -1,14 +1,17 @@
-import "./App.css";
 import { useEffect, useMemo, useState } from "react";
-import Start from "./components/Start";
-import Timer from "./components/Timer";
+import "./App.css";
 import Trivia from "./components/Trivia";
+import Timer from "./components/Timer";
+import Start from "./components/Start";
 
 function App() {
-  const [username, setUsername] = useState(null);
-  const [timeOut, setTimeOut] = useState(false);
+  // only used for project don't usename
+  const [username, setUserName] = useState();
+  // only used for project don't usename
+
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [earned, setEarned] = useState("$ 0");
+  const [stop, setStop] = useState(false);
+  const [earn, setEearn] = useState("$ 0");
 
   const data = [
     {
@@ -77,6 +80,270 @@ function App() {
         },
       ],
     },
+    {
+      id: 4,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
   ];
 
   const moneyPyramid = useMemo(
@@ -86,73 +353,71 @@ function App() {
         { id: 2, amount: "$ 200" },
         { id: 3, amount: "$ 300" },
         { id: 4, amount: "$ 500" },
-        { id: 5, amount: "$ 1.000" },
-        { id: 6, amount: "$ 2.000" },
-        { id: 7, amount: "$ 4.000" },
-        { id: 8, amount: "$ 8.000" },
-        { id: 9, amount: "$ 16.000" },
-        { id: 10, amount: "$ 32.000" },
-        { id: 11, amount: "$ 64.000" },
-        { id: 12, amount: "$ 125.000" },
-        { id: 13, amount: "$ 250.000" },
-        { id: 14, amount: "$ 500.000" },
-        { id: 15, amount: "$ 1.000.000" },
+        { id: 5, amount: "$ 1000" },
+        { id: 6, amount: "$ 2000" },
+        { id: 7, amount: "$ 4000" },
+        { id: 8, amount: "$ 8000" },
+        { id: 9, amount: "$ 16000" },
+        { id: 10, amount: "$ 32000" },
+        { id: 11, amount: "$ 64000" },
+        { id: 12, amount: "$ 125000" },
+        { id: 13, amount: "$ 250000" },
+        { id: 14, amount: "$ 500000" },
+        { id: 15, amount: "$ 1000000" },
       ].reverse(),
     []
   );
 
   useEffect(() => {
     questionNumber > 1 &&
-      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
-  }, [questionNumber, moneyPyramid]);
+      setEearn(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
 
   return (
-    <div className="app">
-      {!username ? (
-        <Start setUsername={setUsername} />
-      ) : (
+    <div className="quiz">
+      {username ? (
         <>
-          <div className="main">
-            {timeOut ? (
-              <h1 className="endText">You earned: {earned}</h1>
+          <div className="quiz_main">
+            {stop ? (
+              <h1 className="quiz_endText">you earned: {earn}</h1>
             ) : (
               <>
-                <div className="top">
-                  <div className="timer">
-                    <Timer
-                      setTimeOut={setTimeOut}
-                      questionNumber={questionNumber}
-                    />
+                <div className="quiz_top">
+                  <div className="quiz_timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
                   </div>
                 </div>
-                <div className="bottom">
+                <div className="quiz_bottom">
                   <Trivia
                     data={data}
+                    setStop={setStop}
                     questionNumber={questionNumber}
                     setQuestionNumber={setQuestionNumber}
-                    setTimeOut={setTimeOut}
                   />
                 </div>
               </>
             )}
           </div>
-          <div className="pyramid">
-            <ul className="moneyList">
+          <div className="quiz_pyramid">
+            <ul className="quiz_moneyList">
               {moneyPyramid.map((m) => (
                 <li
                   className={
                     questionNumber === m.id
-                      ? "moneyListItem active"
-                      : "moneyListItem"
+                      ? "quiz_moneyListItem active"
+                      : "quiz_moneyListItem"
                   }
+                  key={m.id}
                 >
-                  <span className="moneyListItemNumber">{m.id}</span>
-                  <span className="moneyListItemAmount">{m.amount}</span>
+                  <span className="quiz_moneyListItemNumber">{m.id}</span>
+                  <span className="quiz_moneyListItemAmount">{m.amount}</span>
                 </li>
               ))}
             </ul>
           </div>
         </>
+      ) : (
+        <Start setUserName={setUserName} />
       )}
     </div>
   );
